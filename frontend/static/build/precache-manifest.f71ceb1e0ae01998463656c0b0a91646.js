12fc15bbab77a746ccb604490b24cda78bd67aa4
Luke Russell <thelukerussel21@gmail.com>
2020-03-12 13:07:55 -0400

    run build

---
+++ frontend/static/build/precache-manifest.f71ceb1e0ae01998463656c0b0a91646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8855171b8b938ecc84d63bd3017c38a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "176b786fd5a85bb3fafd",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2ee89d422895c7f40cc0",
+    "url": "/static/css/main.0ce6c96a.chunk.css"
+  },
+  {
+    "revision": "176b786fd5a85bb3fafd",
+    "url": "/static/js/2.df883ac0.chunk.js"
+  },
+  {
+    "revision": "60daf3ca32e3efe3fd357caa9175a975",
+    "url": "/static/js/2.df883ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee89d422895c7f40cc0",
+    "url": "/static/js/main.5a57648e.chunk.js"
+  },
+  {
+    "revision": "84c91c1296a21b2dc174",
+    "url": "/static/js/runtime-main.089774d1.js"
+  }
+]);
