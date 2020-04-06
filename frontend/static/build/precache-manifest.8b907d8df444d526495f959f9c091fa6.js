a4b4c10395882406e8306405fae4803840352140
Luke Russell <thelukerussel21@gmail.com>
2020-04-06 10:21:11 -0400

    unregister service worker

--- frontend/static/build/precache-manifest.8b907d8df444d526495f959f9c091fa6.js
+++ frontend/static/build/precache-manifest.8b907d8df444d526495f959f9c091fa6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "681f8da14d3afd06ba7d511281fd57ac",
+    "revision": "45fb8e1848377d889526a3ed8c990e28",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "381f258556c53eaca145",
+    "revision": "4686d4d49fc1f185d491",
     "url": "/static/css/main.47edff13.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.29457ff9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "381f258556c53eaca145",
-    "url": "/static/js/main.63ebd715.chunk.js"
+    "revision": "4686d4d49fc1f185d491",
+    "url": "/static/js/main.3c122fac.chunk.js"
   },
   {
     "revision": "84c91c1296a21b2dc174",
